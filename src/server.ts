837fc1b7b7306b403a93b2ab64ee62ef6d412ebe
Welinton Dias <mdias.welinton@gmail.com>
2022-08-18 18:49:05 -0300

    feat:socket.io add library and handles

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,29 @@
+import { Server } from 'socket.io'
+
 import { app } from './app'
 
-app.listen(3333)
+const appServer = app.listen(3333)
+
+const io = new Server(appServer, {
+  cors: {
+    origin: '*',
+    credentials: true
+  }
+})
+
+global.onlineUsers = new Map()
+
+io.on('connection', (socket) => {
+  global.chatSocket = socket
+
+  socket.on('add-user', (userId) => {
+    global.onlineUsers.set(userId, socket.id)
+  })
+
+  socket.on('send-message', (data) => {
+    const sendUserSocket = global.onlineUsers.get(data.otherUser)
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit('message-recieve', data.message)
+    }
+  })
+})
